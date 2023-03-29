d661a4d76b76cb8ae94dca4dd9d7dd8cb2a82ceb
nikita <nikita.app@icloud.com>
2023-03-30 00:01:38 +0200

    some comments

--- DataLoad.py
+++ DataLoad.py
@@ -2,12 +2,14 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+import shutil
+
 
 class BirdsData:
     def __init__(self, project_folder):
         self.project_folder = project_folder
 
-
+    # simply retrieve the features and corresponding label for given sample of specified feature
     def get_sound_label(self, species, sample):
         folder_path = f'{self.project_folder}/{species}'
         file_list = os.listdir(f'{self.project_folder}/{species}')
@@ -17,6 +19,10 @@ class BirdsData:
                 data = np.load(f'{file_path}.npy')
                 label = np.load(f'{file_path}.labels.npy')
                 return np.array(data), np.array(label)
+
+    # potential function to complete "annotators agreement" part. Returns general agreement, positive agreement (how
+    # many annotators think positive class to be positive, negative agreement (how many annotators think "others" part
+    # to be "others"
 
     def compute_agreement(self, species):
         folder_path = f'{self.project_folder}/{species}'
@@ -29,7 +35,7 @@ class BirdsData:
         general_agreements = 0
         for i in range(len(labels_ar_list)):
             for j in range(0, 100):
-                                                              #index of label which occurs the most
+                # index of label which occurs the most
                 general_agreements += np.mean(labels_ar_list[i][j] == np.argmax(np.bincount(labels_ar_list[i][j][1:])))
 
         positive_agreements = 0
@@ -39,17 +45,20 @@ class BirdsData:
         for i in range(len(labels_ar_list)):
             for j in range(0, 100):
                 if labels_ar_list[i][j][0] == 0:
-                    negative_agreements += np.mean(labels_ar_list[i][j] == np.argmax(np.bincount(labels_ar_list[i][j][1:])))
+                    negative_agreements += np.mean(
+                        labels_ar_list[i][j] == np.argmax(np.bincount(labels_ar_list[i][j][1:])))
                     count_n += 1
                 else:
-                    positive_agreements += np.mean(labels_ar_list[i][j] == np.argmax(np.bincount(labels_ar_list[i][j][1:])))
+                    positive_agreements += np.mean(
+                        labels_ar_list[i][j] == np.argmax(np.bincount(labels_ar_list[i][j][1:])))
                     count_p += 1
 
-        general_agreements = general_agreements/(len(labels) * 100)
-        positive_agreements = positive_agreements/count_p
-        negative_agreements = negative_agreements/count_n
+        general_agreements = general_agreements / (len(labels) * 100)
+        positive_agreements = positive_agreements / count_p
+        negative_agreements = negative_agreements / count_n
         return np.array([general_agreements, positive_agreements, negative_agreements])
 
+    # Method below might be of use to further calculate which features are important and which are not.
 
     def compute_feature_correlations_for_species(self, species):
         folder_path = f'{self.project_folder}/{species}'
@@ -61,15 +70,9 @@ class BirdsData:
             cor_mat_list.append(np.corrcoef(np.load(file_paths[i]).T))
         return np.mean(cor_mat_list, axis=0)
 
+    def label_characteristics(self):
+        # firstly remove all 0 labels to seperate folder
+        alles = []
 
 
 
-
-print(BirdsData('ptichki').get_sound_label('comcuc', '25627'))
-print(BirdsData('ptichki').compute_agreement('comcuc'))
-print(BirdsData('ptichki').compute_agreement('cowpig1'))
-print(BirdsData('ptichki').compute_agreement('eucdov'))
-print(BirdsData('ptichki').compute_agreement('eueowl1'))
-print(BirdsData('ptichki').compute_agreement('grswoo'))
-print(BirdsData('ptichki').compute_agreement('tawowl1'))
-print('Hello world')
