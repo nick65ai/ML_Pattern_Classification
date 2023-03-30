0c695b95b8d961474a498694d9b6781ab0168a41
nikita <nikita.app@icloud.com>
2023-03-30 20:28:14 +0200

    some comments

--- BirdExplore.py
+++ BirdExplore.py
@@ -1,8 +1,9 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-import glob
-
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import re
 
 class BirdsData:
     def __init__(self, project_folder):
@@ -28,42 +29,42 @@ class BirdsData:
 
 
         comcuc = [f for f in all_files if 'comcuc' in f]
-        comcuc_features = [''.join(filter(str.isdigit, file_path.split('/')[-1])) for file_path in comcuc]
+        comcuc_features = [str(re.search(r"\d+(?=.npy)", s).group(0)) for s in comcuc]
         comcuc_ars = []
         for f in comcuc_features:
             comcuc_ars.append(BirdsData(self.project_folder).get_sound_label('comcuc', f))
         comcuc_ars = np.concatenate(comcuc_ars)
 
         cowpig1 = [f for f in all_files if 'cowpig1' in f]
-        cowpig1_features = [''.join(filter(str.isdigit, file_path.split('/')[-1])) for file_path in cowpig1]
+        cowpig1_features = [str(re.search(r"\d+(?=.npy)", s).group(0)) for s in cowpig1]
         cowpig1_ars = []
         for f in cowpig1_features:
             cowpig1_ars.append(BirdsData(self.project_folder).get_sound_label('cowpig1', f))
         cowpig1_ars = np.concatenate(cowpig1_ars)
 
         eucdov = [f for f in all_files if 'eucdov' in f]
-        eucdov_features = [''.join(filter(str.isdigit, file_path.split('/')[-1])) for file_path in eucdov]
+        eucdov_features = [str(re.search(r"\d+(?=.npy)", s).group(0)) for s in eucdov]
         eucdov_ars = []
         for f in eucdov_features:
             eucdov_ars.append(BirdsData(self.project_folder).get_sound_label('eucdov', f))
         eucdov_ars = np.concatenate(eucdov_ars)
 
         eueowl1 = [f for f in all_files if 'eueowl1' in f]
-        eueowl1_features = [''.join(filter(str.isdigit, file_path.split('/')[-1])) for file_path in eueowl1]
+        eueowl1_features = [str(re.search(r"\d+(?=.npy)", s).group(0)) for s in eueowl1]
         eueowl1_ars = []
         for f in eueowl1_features:
             eueowl1_ars.append(BirdsData(self.project_folder).get_sound_label('eueowl1', f))
         eueowl1_ars = np.concatenate(eueowl1_ars)
 
         grswoo = [f for f in all_files if 'grswoo' in f]
-        grswoo_features = [''.join(filter(str.isdigit, file_path.split('/')[-1])) for file_path in grswoo]
+        grswoo_features = [str(re.search(r"\d+(?=.npy)", s).group(0)) for s in grswoo]
         grswoo_ars = []
         for f in grswoo_features:
             grswoo_ars.append(BirdsData(self.project_folder).get_sound_label('grswoo', f))
         grswoo_ars = np.concatenate(grswoo_ars)
 
         tawowl1 = [f for f in all_files if 'tawowl1' in f]
-        tawowl1_features = [''.join(filter(str.isdigit, file_path.split('/')[-1])) for file_path in tawowl1]
+        tawowl1_features = [str(re.search(r"\d+(?=.npy)", s).group(0)) for s in tawowl1]
         tawowl1_ars = []
         for f in tawowl1_features:
             tawowl1_ars.append(BirdsData(self.project_folder).get_sound_label('tawowl1', f))
@@ -72,7 +73,34 @@ class BirdsData:
 
         all_birdiiies = np.concatenate((comcuc_ars, cowpig1_ars, eucdov_ars, eueowl1_ars, grswoo_ars, tawowl1_ars))
 
-        return all_birdiiies.shape
+        return all_birdiiies
+
+
+    def pca(self):
+        scaler = StandardScaler()
+        dataset = BirdsData(self.project_folder).united_dataset()
+        X = dataset[:, 0:-1]
+        y = dataset[:, -1]
+        scaler.fit(X)
+        X_scaled = scaler.transform(X)
+
+    def labels_distribution(self):
+        dataset = BirdsData(self.project_folder).united_dataset()
+        others_count = len(dataset[dataset[:, -1] == 0])
+        class_counts = []
+        for i in range(0, 7):
+            class_counts.append(len(dataset[dataset[:, -1] == i]))
+
+        plt.figure(figsize=(10, 7))
+        x = ['other', 'comcuc', 'cowpig1', 'eucdov', 'eueowl1', 'grswoo', 'tawowl1']
+        y = class_counts
+        plt.plot(x, y)
+        plt.title('label distribution')
+        plt.show()
+
+        return class_counts
+
+
 
 
 
@@ -132,7 +160,7 @@ class BirdsData:
 
 
     def plot_cor_distribution(self, species, save = False):
-        plt.figure(figsize=(14,10))
+        plt.figure(figsize=(14, 10))
         y = np.unique(BirdsData(self.project_folder).compute_feature_correlations_for_species(species))
         plt.plot(y)
         plt.grid()
@@ -142,5 +170,4 @@ class BirdsData:
 
 
 
-a = BirdsData('ptichki').get_sound_label('eucdov', '748338')
-print(BirdsData('ptichki').united_dataset())
+print(BirdsData('ptichki').labels_distribution())
